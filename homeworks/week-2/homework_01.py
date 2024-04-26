0a09a75aa49951b2bd7b2bd4c329be8e82aef7c5
Gil Dantas <gipimapa@gmail.com>
2024-04-26 18:31:55 +0200

    Test pushing

--- homeworks/week-2/homework_01.py
+++ homeworks/week-2/homework_01.py
@@ -2,7 +2,6 @@
 def sum_numbers(n, m):
     # ToDo: write your code here
     
-
 if __name__ == '__main__':
     # ToDo: Ask user to provide the values and store them in variables.
     sum_numbers(first_number, second_number)
