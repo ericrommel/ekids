0d664fccd6705fff34abfb024f484cca1354cb05
Alexandr Boykov <aboykov2000@gmail.com>
2024-05-02 17:36:06 +0200

    Update homework_01.py

---
+++ homeworks/week-3/homework_01.py
@@ -0,0 +1,26 @@
+"""
+In this game, we'll create a number pyramid where each row contains increasing numbers. E.g.: If user types 5, the game
+prints:
+
+Number Pyramid:
+    1
+   1 2
+  1 2 3
+ 1 2 3 4
+1 2 3 4 5
+"""
+
+
+def number_pyramid(rows):
+    print("Number Pyramid:")
+    for i in range(1, rows + 1):
+        # ToDo: Print spaces to center-align the pyramid
+        print()
+        # ToDo: Print numbers in increasing order
+        for j in range(1, i + 1):
+            print(1, 2, 3, 4, 5, sep=" ")
+
+
+# ToDo: Ask user to type a number
+number = int(input("Enter the number: "))
+number_pyramid(number)
