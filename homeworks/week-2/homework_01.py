ef1966cb52e84cadfc0e915239cb8967b954c8c8
cuteness1234 <tofunmi984@gmail.com>
2024-05-03 15:36:35 +0200

    This is my homework

--- homeworks/week-2/homework_01.py
+++ homeworks/week-2/homework_01.py
@@ -2,11 +2,10 @@
 def sum_numbers(n, m):
     # ToDo: write your code here
     j = n + m
-
     print(j)
 
 if __name__ == '__main__':
     # ToDo: Ask user to provide the values and store them in variables.
-    first_number = 3
-    second_number = 3
+    first_number = int(input("Add first number"))
+    second_number = int(input("Add second number"))
     sum_numbers(first_number, second_number)
