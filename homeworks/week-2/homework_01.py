f601ba428f9e802361239fed9f7404643b23b0cb
cuteness1234 <tofunmi984@gmail.com>
2024-05-03 15:25:38 +0200

    Your commit message here

--- homeworks/week-2/homework_01.py
+++ homeworks/week-2/homework_01.py
@@ -1,7 +1,12 @@
 # Make a program that asks for two integers and prints out the sum of those two numbers.
 def sum_numbers(n, m):
     # ToDo: write your code here
+    j = n + m
+
+    print(j)
 
 if __name__ == '__main__':
     # ToDo: Ask user to provide the values and store them in variables.
+    first_number = 3
+    second_number = 3
     sum_numbers(first_number, second_number)
