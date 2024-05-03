f2d387a9bdb367fa9150c1cb04ef754db868b3a9
MaksymPotapov <potapov.max06@gmail.com>
2024-05-03 19:45:53 +0200

    homework week 2 (6)

--- homeworks/week-2/homework_06.py
+++ homeworks/week-2/homework_06.py
@@ -1,7 +1,11 @@
 # Do it the other way around, from Fahrenheit to Celsius.
-def farenheit_to_celcius(c):
+def fahrenheit_to_celsius(f):
     # ToDo: write your code here
+    temperature_in_celsius = (f-32)*5/9
+    print(f'\n\n{'-'*50}\nYour temperature in celsius is: {round(temperature_in_celsius, 2)}C')
+
 
 if __name__ == '__main__':
     # ToDo: Ask user to provide the values and store them in variables.
-    farenheit_to_celcius(temperature_in_fahrenheit)
+    temperature_in_fahrenheit = int(input('Type your temperature in fahrenheit\t'))
+    fahrenheit_to_celsius(temperature_in_fahrenheit)
