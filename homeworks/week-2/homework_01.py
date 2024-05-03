1b2222a1f791e87c62cd7044eeef9b4bed8c26cc
MaksymPotapov <potapov.max06@gmail.com>
2024-05-03 19:14:05 +0200

    Testing the push

--- homeworks/week-2/homework_01.py
+++ homeworks/week-2/homework_01.py
@@ -1,7 +1,10 @@
 # Make a program that asks for two integers and prints out the sum of those two numbers.
 def sum_numbers(n, m):
     # ToDo: write your code here
+    print(f'\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nThe answer is: {n + m}')
 
 if __name__ == '__main__':
     # ToDo: Ask user to provide the values and store them in variables.
+    first_number = int(input('Type the first number to sum\t'))
+    second_number = int(input('Type the second number\t'))
     sum_numbers(first_number, second_number)
