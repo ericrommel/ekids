c4cd5892e3200c24d14c6a4920f37fac69dcd5a5
Maksym <128859767+ZenIx-why@users.noreply.github.com>
2024-05-10 19:49:10 +0200

    Update cypher.py
    Updated program so now user can enter their own message

--- classes/repetition_structures/cypher.py
+++ classes/repetition_structures/cypher.py
@@ -1,21 +1,37 @@
-'''
-def hello_world(text):
-    print(text)
-
-hello_world("Have fun!")
-print("Let's see the next...")
-hello_world("My name is?")
-
-'''
-
-print("WELCOME TO A SECRET MESSAGE PROGRAMM!")
-
+#Function to encrypt the word
 def encrypt(word):
     new_word = ""
     for i in word:
-        print(ord(i))
-        new_word = new_word + chr(ord(i) + 3)
+        ascii_number = ord(i)
+        print(f"My ASCII number is {ascii_number}")
+        key = user_key
+        print(f"My key is {key}")
+        new_number = ascii_number + key
+        print(f"My new number is {new_number}")
+        new_character = chr(new_number)
+        new_word = new_word + new_character
     return new_word
 
-message = input("Enter your your message that you want to be converted: ")
-print(encrypt(message))
+#Function to decrypt the word
+def decrypt(message):
+    original_word = ""
+    for i in message:
+        ascii_number_s = ord(i)
+        key = user_key
+        ascii_number_o = ascii_number_s - key
+        ascii_character = chr(ascii_number_o)
+        original_word = original_word + ascii_character
+    return original_word
+
+
+#Getting User's data
+user_message = input("Eneter your message: ")
+user_key = int(input("Enter your key in number only: "))
+
+#Using the functions to proccess
+encrypted_message = encrypt(user_message)
+decrypted_message = decrypt(encrypted_message)
+
+#Printing the data
+print(f"Encrypted message: {encrypted_message}")
+print(f"Decrypted message: {decrypted_message}")
