e516da6b83bf71d104c6cdc9a89c23632d1a7c4e
ZenIx <max.starodubtsev@icloud.com>
2024-05-11 10:37:40 +0200

    Finished the homework all 4 stages

--- classes/repetition_structures/cypher.py
+++ classes/repetition_structures/cypher.py
@@ -1,35 +1,38 @@
 #Function to encrypt the word
-def encrypt(word):
+def encrypt(word, key):
     secret_word = ""
     '''
     HOMEWORK:
     ToDo: Improve this code to:
     0. Ask the user to provide key value and the message. Make sure to have special character
-    on it (because you want to test it, right?)
-    1. Only use letters from A to Z (in a big letters)
-    2. If there is a special character, keep it as it is. E.g. 'EP;AM' -> 'JU;FR'
-    3. If the character is a SPACE, keep the space as well. E.g. 'I am a legend' -> 'N fr f gjljsi' 
+    on it (because you want to test it, right?)                                                         1/1
+    1. Only use letters from A to Z (in a big letters)                                                  1/1
+    2. If there is a special character, keep it as it is. E.g. 'EP;AM' -> 'JU;FR'                       1/1
+    3. If the character is a SPACE, keep the space as well. E.g. 'I am a legend' -> 'N fr f gjljsi'     1/1
     '''
+    word = word.upper()
     for i in word:
-        ascii_number = ord(i)
-        print(f"My ASCII number is {ascii_number}")
-        key = user_key
-        print(f"My key is {key}")
-        new_number = ascii_number + key
-        print(f"My new number is {new_number}")
-        new_character = chr(new_number)
-        secret_word = secret_word + new_character
+        if i.isalpha():  # Check if it's a letter
+            ascii_number = ord(i.upper())
+            new_number = (ascii_number - 65 + user_key) % 26 + 65  # Shift within A-Z range
+            new_character = chr(new_number)
+        else:
+            new_character = i  # Keep non-alphabetic characters as they are
+        secret_word += new_character
     return secret_word
 
 #Function to decrypt the word
-def decrypt(message):
+def decrypt(message, key):
+    message = message.upper()
     original_word = ""
     for i in message:
-        ascii_number_secret = ord(i)
-        key = user_key
-        ascii_number_original = ascii_number_secret - key
-        ascii_character = chr(ascii_number_original)
-        original_word = original_word + ascii_character
+        if i.isalpha():
+            ascii_number_secret = ord(i.upper())
+            ascii_number_original = (ascii_number_secret - 65 - key) % 26 + 65
+            original_character = chr(ascii_number_original)
+        else:
+            original_character = i
+        original_word += original_character
     return original_word
 
 #How to check if the number is in the range of numbers
@@ -45,8 +48,8 @@ user_message = input("Eneter your message: ")
 user_key = int(input("Enter your key in number only: "))
 
 #Using the functions to proccess
-encrypted_message = encrypt(user_message)
-decrypted_message = decrypt(encrypted_message)
+encrypted_message = encrypt(user_message, user_key)
+decrypted_message = decrypt(encrypted_message, user_key)
 
 #Printing the data
 print(f"Encrypted message: {encrypted_message}")
