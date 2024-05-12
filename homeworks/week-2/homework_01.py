a38a3b1e311f73ad7dff25886cf1daeeb60ee701
Caio Rommel <caiorommelsd@gmail.com>
2024-05-12 11:55:25 +0200

    Solve homework 1

--- homeworks/week-2/homework_01.py
+++ homeworks/week-2/homework_01.py
@@ -1,7 +1,9 @@
 # Make a program that asks for two integers and prints out the sum of those two numbers.
 def sum_numbers(n, m):
-    # ToDo: write your code here
+    print(f"The sum of {n} and {m} is {n + m}")
     
 if __name__ == '__main__':
     # ToDo: Ask user to provide the values and store them in variables.
+    first_number = int(input("Type a first number: ")
+    second_number = int(input("Type a second number: ")
     sum_numbers(first_number, second_number)
