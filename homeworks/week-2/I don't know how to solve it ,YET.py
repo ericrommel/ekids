8d34ad04a6e5615e806dab6a808447e9d2a8aa59
MaksymPotapov <potapov.max06@gmail.com>
2024-05-31 20:17:37 +0200

    hi

--- homeworks/week-2/I don't know how to solve it ,YET.py
+++ homeworks/week-2/I don't know how to solve it ,YET.py
@@ -1,2 +1,10 @@
 # CREATE A PROGRAM TO COUNT HOW MANY UNIQUE LETTERS AND THEIR QUANTITY FROM A PHRASE!
 # E.G; HELLO WORLD!
+
+my_dict = {}
+phrase = "I do not know what I am doing at all.I need help, PLEASE"
+print(len())
+
+
+
+
--- homeworks/week-2/homework_05.py
+++ homeworks/week-2/homework_05.py
@@ -1,10 +1,13 @@
 # Convert a typed temperature in Celsius to Fahrenheit. F = 9*C/5 + 32
-def celcius_to_farenheit(c):
+def celsius_to_fahrenheit(C):
     # ToDo: write your code here
 
 if __name__ == '__main__':
     # ToDo: Ask user to provide the values and store them in variables.
+c = celsius_to_fahrenheit
+    F = 9 * c / 5 + 32
 
-    temperature_in_celcius = int(input("Add temperature_in_celcius and press enter : "))
 
-    celcius_to_farenheit(temperature_in_celcius)
+    temperature_in_celsius = int(input("Add temperature_in_celsius and press enter : "))
+
+    celcius_to_fahrenheit(temperature_in_celsius)
