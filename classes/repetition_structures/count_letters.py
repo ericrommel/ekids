deeccb659b31c9949ca46e5e6fb6e870e42472d3
MaksymPotapov <potapov.max06@gmail.com>
2024-05-31 20:02:36 +0200

    count the use of each letter in a phrase

---
+++ classes/repetition_structures/count_letters.py
@@ -0,0 +1,13 @@
+import re  # regex - regex101.com
+
+phrase = "Hello.World!"
+my_dict = {}
+special_ch = ['!', '.', ' ']
+for i in phrase:
+    if i not in special_ch:
+        if i in my_dict:  # in my_dict.values() if you want to check if it's there as a value
+            my_dict[i] += 1
+        else:
+            my_dict[i] = 1
+for k, v in my_dict.items():
+    print(f"{k}: {v} times")
